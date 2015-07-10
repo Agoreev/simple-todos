96447fe9de632378b8dac7fd186c4a772096fe14
Agoreev <goreev.ar@gmail.com>
2015-07-10 16:07:16 +0300

    Initial commit

---
+++ simple-todos.js
@@ -0,0 +1,56 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Template.body.helpers({
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      console.log(event);
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+
+      return false;
+    },
+
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
